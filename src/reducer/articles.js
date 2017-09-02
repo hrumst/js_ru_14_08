50158e2dc5daeb79e0985069f780ae6c1d4b7ffe
andrey <andrey.shlygin@goods.ru>
2017-09-02 15:48:34 +0300

    add filter reducer and filter constant

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,12 +1,25 @@
-import { DELETE_ARTICLE } from '../constants'
+import { DELETE_ARTICLE, FILTER_ARTICLES_BY_ID } from '../constants'
 import {articles as defaultArticles} from '../fixtures'
 
 export default (articles = defaultArticles, action) => {
-    const { type, payload } = action
+    const { type, payload } = action;
 
     switch (type) {
+
         case DELETE_ARTICLE:
-            return articles.filter(article => article.id !== payload.id)
+            return articles.filter(article => article.id !== payload.id);
+
+        case FILTER_ARTICLES_BY_ID:
+
+            if (!Array.isArray(payload.ids) || payload.ids.length < 1) {
+                return defaultArticles;
+            }
+
+            return defaultArticles.filter(article => {
+
+                return payload.ids.indexOf(article.id) !== -1;
+            });
+
     }
 
     return articles
